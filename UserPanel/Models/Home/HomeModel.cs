bfe7cae3be056a3c43341d07529393bac4d6742c
kacper <kacper@DESKTOP-0R3GA0R>
2024-03-25 21:00:39 +0100

    filter campaning cd..

--- UserPanel/Models/Home/HomeModel.cs
+++ UserPanel/Models/Home/HomeModel.cs
@@ -4,6 +4,5 @@ namespace UserPanel.Models.Home
     public class HomeModel
     {
         public FilterParametr FilterParametr { get; set; }
-        public List<Campaning.Campaning>? campaningsUser { get; set; }
     }
 }
