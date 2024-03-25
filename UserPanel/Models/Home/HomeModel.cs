5192e04f764fcc257339de00ff8e7af1396ef322
kacper <kacper@DESKTOP-0R3GA0R>
2024-03-25 19:59:02 +0100

    post select campaning guid home

--- UserPanel/Models/Home/HomeModel.cs
+++ UserPanel/Models/Home/HomeModel.cs
@@ -1,7 +1,9 @@
-﻿namespace UserPanel.Models.Home
+﻿using UserPanel.Models.Campaning;
+namespace UserPanel.Models.Home
 {
     public class HomeModel
     {
         public FilterParametr FilterParametr { get; set; }
+        public List<Campaning.Campaning>? campaningsUser { get; set; }
     }
 }
