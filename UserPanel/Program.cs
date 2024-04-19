071291458b71ab38cd51b7999426fc306f27a3a6
kacper <kacper@DESKTOP-0R3GA0R>
2024-04-19 23:44:45 +0200

    program.cs refactor

--- UserPanel/Program.cs
+++ UserPanel/Program.cs
@@ -1,44 +1,35 @@
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi.Models;
-using System.Security.Claims;
-using System.Security.Cryptography;
 using UserPanel.Helpers;
 using UserPanel.Installers;
-using UserPanel.Interfaces;
 using UserPanel.Middleware;
-using UserPanel.Models.Config;
-using UserPanel.Providers;
-using UserPanel.References;
 using UserPanel.Services;
-using UserPanel.Services.database;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.InstallServices();
+
 ConfigurationHelper.Initialize(builder.Configuration);
 ConfigManager.LoadConfig();
+
 var app = builder.Build();
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 app.UseSwagger();
+
 app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "AddingStuffAndChecking v1");
 });
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+
 app.UseMiddleware<PageTypeMiddleware>();
+
 app.UseAuthentication();
 app.UseAuthorization();
 app.UseSession();
